da2a4ffd60ab246638feeb564ea0e8915cd76949
michel <michel.gds@hotmail.com>
2019-11-26 15:29:24 -0300

    Save user route

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import User from './app/controllers/UserController';
 
-import RankingController from './app/controllers/RankingController'
+import RankingController from './app/controllers/RankingController';
 
 const routes = new Router();
 
 routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
 routes.get('/ranking', RankingController.index);
+routes.post('/store', User.store);
 
 export default routes;
