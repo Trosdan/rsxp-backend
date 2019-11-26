faead7285a2de7cd6008ba4d193829af2b92c6f5
Jordan Oliveira <jordandossantosoliveira@gmail.com>
2019-11-26 15:01:13 -0300

    WIP add score

---
+++ src/app/controllers/ScoreController.js
@@ -0,0 +1,11 @@
+class ScoreController {
+  async store(req, res) {
+    const { userId, score } = req.queryParams;
+
+    // const user = User.find(userId);
+    // const newUser = {...user , score: score + user.score};
+
+    //
+
+  }
+}
